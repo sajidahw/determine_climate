b53f792fbb5ed1dff7792654f9ead3fabe025185
sajidahw <sajidahwahdy@Sajidahs-MacBook-Air.local>
2024-02-07 14:27:30 -0800

    installed dependencies, began react skeleton code, created citysearch component, updated css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import CitySearch from './components/citysearch/citysearch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <CitySearch />
     </div>
   );
 }
 
 export default App;
+
+// importing our CitySearch component via <CitySearch /> in the App component
---
+++ src/components/citysearch/citysearch.js
@@ -0,0 +1,9 @@
+// Search component for city search on top of the screen using the async paginate package
+
+
+const CitySearch = () => {
+    // dummy component
+    return ("Hello World!")
+}
+
+export default CitySearch;
