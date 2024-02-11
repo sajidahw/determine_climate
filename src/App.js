c4bffce0c52c39c346732b94871380ad0d90ceda
sajidahw <sajidahwahdy@Sajidahs-MacBook-Air.local>
2024-02-10 18:55:14 -0800

    updated driver app.js file for rendering and connection of components, styling and added API urls and keys

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
+import "./App.css";
+import CitySearch from "./components/citysearch/citysearch"; // component passing prop onSearchCityChange to pass data
+import ZipcodeSearch from "./components/zipsearch/zipsearch";
+// import AutocompleteGoogleMaps from "./components/googlemaps/autocompleteGoogleMaps";
+import ActualWeather from "./components/actual_weather/actual_weather";
+import { OPENWEATHER_API_URL, OPENWEATHER_API_KEY } from "./api";
 
-import './App.css';
-import CitySearch from './components/citysearch/citysearch'; // component passing prop onSearchCityChange to pass data
-import AutocompleteGoogleMaps from './components/googlemaps/autocompleteGoogleMaps';
-import ActualWeather from './components/actual_weather/actual_weather';
-import { OPENWEATHER_API_URL, OPENWEATHER_API_KEY } from './api';
- // eslint-disable-next-line
-import { GOOGLEMAPS_API_KEY, googlemaps_api_url } from './api';
-import { useState } from 'react';
-
+// eslint-disable-next-line
+import { GOOGLEMAPS_API_KEY, googlemaps_api_url } from "./api";
+import { useState } from "react";
 
 // placing components on to main page
 function App() {
+  // *** CITY SEARCH **
+  // 2 hooks to store the 2 fetches to see basic weather card data
+  const [currentWeather, setCurrentWeather] = useState(null); // initial value to be empty
+  const [forecastWeather, setForecastWeather] = useState(null); // forecastWeather never used
 
-  // 2 hooks to store the 2 fetches
-  const[currentWeather, setCurrentWeather] = useState(null); // initial value to be empty
-  const[forecastWeather, setForecastWeather] = useState(null);
-  
-  const handleOnCitySearchChange = (searchCityData) => {  
+  const handleOnCitySearchChange = (searchCityData) => {
     const [lat, lon] = searchCityData.value.split(" ");
 
     // 2 API calls in promise fetch for current weather and then forecast from OpenWeather API code
@@ -24,10 +24,14 @@ function App() {
 
     // 1st fetch for current weather, truncate API call to create constant in api.js; replace {} w/${} & import
     // replace {lat},{lon} with our defined vars above${lat},${lon
-    const currentWeatherAPIFetch = fetch(`${OPENWEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}&units=imperial`);
+    const currentWeatherAPIFetch = fetch(
+      `${OPENWEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}&units=imperial`
+    );
 
     // 2nd fetch for forecast [based off of API doc code]
-    const forecastWeatherAPIFetch = fetch(`${OPENWEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}&units=imperial`);
+    const forecastWeatherAPIFetch = fetch(
+      `${OPENWEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}&units=imperial`
+    );
 
     // order is important; promise.all will wait for both fetches to complete before moving on
     Promise.all([currentWeatherAPIFetch, forecastWeatherAPIFetch])
@@ -37,40 +41,119 @@ function App() {
         const forecastWeatherResponse = await response[1].json();
 
         // update the responses to the state via setVar so data can be passed to the components & using spread operator to add data from citysearch.js
-        setCurrentWeather({ city: searchCityData.label, ...currentWeatherResponse}); // pulling from vars above & adding data from citysearch.js so labels appear
-        setForecastWeather({ city: searchCityData.label, ...forecastWeatherResponse});
+        setCurrentWeather({
+          city: searchCityData.label,
+          ...currentWeatherResponse,
+        }); // pulling from vars above & adding data from citysearch.js so labels appear
+        setForecastWeather({
+          city: searchCityData.label,
+          ...forecastWeatherResponse,
+        });
       })
-      .catch((error) => console.log(error));// catch any errors if it fails
+      .catch((error) => console.log(error)); // catch any errors if it fails
 
     // experiment with this, check API doc and test if it works
     // const cityLocationFetch = fetch(`${googlemaps_api_url}?key=${GOOGLEMAPS_API_KEY}&libraries=places`)
 
     // console.log(searchCityData); // log the searchCityData to the console
-  }
-  
- // testing to see if get current weather and forecast
-  console.log(currentWeather);
-  console.log(forecastWeather);
+  };
 
-// passing in component if no errors by using &&
+  // testing to see if get current weather and forecast
+  // console.log(currentWeather);
+  // console.log(forecastWeather);
+
+  // ***ZIPCODE SEARCH**
+  // 2 hooks to store the 2 fetches to see basic weather card data
+  const [currentZipcodeWeather, setCurrentZipcodeWeather] = useState(null);
+  const [forecastZipcodeWeather, setForecastZipcodeWeather] = useState(null);
+
+  const handleOnZipcodeSearchChange = (searchZipcodeData) => {
+    const { lat, lon } = searchZipcodeData;
+
+    // 2 API calls in promise fetch for current weather and then forecast from OpenWeather API code
+    // 2 fetches will be stored into variables to pass to the promise array
+
+    // 1st fetch for current weather, truncate API call to create constant in api.js; replace {} w/${} & import; code adapted from OpenWeather API doc
+    // replace {lat},{lon} with defined vars above${lat},${lon
+    const currentZipcodeWeatherAPIFetch = fetch(
+      `${OPENWEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}&units=imperial`
+    );
+
+    // 2nd fetch for forecast [based off of API doc code]
+    const forecastZipcodeWeatherAPIFetch = fetch(
+      `${OPENWEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}&units=imperial`
+    );
+
+    // order is important; promise.all will wait for both fetches to complete before moving on
+    Promise.all([currentZipcodeWeatherAPIFetch, forecastZipcodeWeatherAPIFetch])
+      .then(async (response) => {
+        // map which data we are getting in JSON from API fetches
+        const currentZipcodeWeatherResponse = await response[0].json();
+        const forecastZipcodeWeatherResponse = await response[1].json();
+      })
+      .catch((error) => console.log(error)); // catch any errors if it fails
+
+    // experiment with this, check API doc and test if it works
+    // const cityLocationFetch = fetch(`${googlemaps_api_url}?key=${GOOGLEMAPS_API_KEY}&libraries=places`)
+
+    // console.log(searchCityData); // log the searchCityData to the console
+  };
+
+  // testing to see if get current weather and forecast
+  // console.log(currentZipcodeWeather);
+  // console.log(forecastZipcodeWeather);
+
+  // previously had this in the return statement below
+  /* <ZipcodeSearch onSearchZipcodeChange={handleOnZipcodeSearchChange} /> 
+      {currentZipcodeWeather && <ActualWeather data={ currentZipcodeWeather}/>} */
+
+  // passing in component if no errors by using && to feed to weather card in ActualWeather.js
   return (
-    <div className="inputcontainer">
-      <CitySearch onSearchCityChange={handleOnCitySearchChange} /> 
-      {currentWeather && <ActualWeather data={ currentWeather}/>}
+    <>
+      <h1>Weather Retrieval App</h1>
+      <h3>
+        Greetings, retrieve weather for your selected location by entering a
+        city's name or a U.S. zipcode.
+      </h3>
 
-      <div className='mapLabel'>
-        <h2> Find and Display a Location</h2>
+      <div className="inputcontainer">
+        {currentWeather && <ActualWeather data={currentWeather} />}
+        <div></div>
 
-        <div className='gmap'>
-          <AutocompleteGoogleMaps />
-        </div>
-        {/* <AutocompleteGoogleMaps /> */}
+        <button
+          className="weatherCardButton"
+          onClick={() => setCurrentWeather("")}
+        >
+          Remove Weather
+        </button>
+        <div></div>
+        <div></div>
+
+        <CitySearch onSearchCityChange={handleOnCitySearchChange} />
+
+        <ZipcodeSearch
+          setCurrentWeather={setCurrentWeather}
+          setForecastWeather={setForecastWeather}
+        />
       </div>
 
-    </div>
+      <footer>
+        <p>&copy; Sajidah W | Winter 2024 | REACT Weather App</p>
+      </footer>
+    </>
   );
 }
 
 export default App;
 
-// importing our CitySearch component with added event via <CitySearch /> in the App component
+// importing CitySearch component with added event via <CitySearch /> in the App component
+
+// inside div "inputcontainer" above when reinstating autocomplete Google Maps
+
+/* <div className="mapLabel">
+  <h2> Find and Display a Location</h2>
+
+  <div className="gmap">
+    <AutocompleteGoogleMaps />
+  </div>
+</div>; */
--- src/api.js
+++ src/api.js
@@ -1,21 +1,28 @@
-// geodb api key and host to extract city 
+// geodb api key and host to extract city
 
-
-export const geodb_api_url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geodb_api_url = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const geodbApiOptions = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_X_RAPIDAPI_KEY,
-        'X-RapidAPI-Host': process.env.REACT_APP_X_RAPIDAPI_HOST,
-    }
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_X_RAPIDAPI_KEY,
+    "X-RapidAPI-Host": process.env.REACT_APP_X_RAPIDAPI_HOST,
+  },
 };
 
-export const googlemaps_api_url = 'https://maps.googleapis.com/maps/api/js';
-export const GOOGLEMAPS_API_KEY = process.env.REACT_APP_GOOGLEMAPS_API_KEY;
-
-export const libraries = ['places'];
-
-export const OPENWEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+// OpenWeather API key
 
 export const OPENWEATHER_API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
+
+// OpenWeather API URL for relaying weather data
+export const OPENWEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+// zipcode OpenWeather API zipcode search conversion to lat/lon
+export const OPENWEATHER_API_URL_ZIPCODE_CONVERSION =
+  "http://api.openweathermap.org/geo/1.0";
+
+// Google Maps API key and URL
+export const googlemaps_api_url = "https://maps.googleapis.com/maps/api/js";
+export const GOOGLEMAPS_API_KEY = process.env.REACT_APP_GOOGLEMAPS_API_KEY;
+
+export const libraries = ["places"];
