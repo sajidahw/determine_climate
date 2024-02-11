8fc3baf92a92684e3db2c46d52f8707666c0b31c
sajidahw <sajidahwahdy@Sajidahs-MacBook-Air.local>
2024-02-10 19:03:44 -0800

    updated citysearch and zipsearch components

--- src/components/citysearch/citysearch.js
+++ src/components/citysearch/citysearch.js
@@ -1,53 +1,80 @@
 // Search component for city search on top of the screen using the async paginate package
+// driven by geodb API for city search to obtain lat/long data
 
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { geodbApiOptions, geodb_api_url } from "../../api";
 
-
 // action which drives the city search in passing data to other widgets
 const CitySearch = ({ onSearchCityChange }) => {
-    // use state hooks variables with set for updating variable
-    const [citysearch, setCitySearch] = useState(null); // initial state is null
+  // use state hooks variables with set for updating variable
+  const [citysearch, setCitySearch] = useState(null); // initial state is null
 
-    // using fetch method to url to retrieve data from GeoDB API; adding constraint to show cities with population over 10,000 and showing results of inputValue
-    const loadCityOptions = (inputValue) => {
-
-        // promise chain instead of async/await
-        // inner return: return array of objects with long & lat as OpenWeatherMap API requires it to retrieve weather data
-        return fetch(
-            `${geodb_api_url}/cities?minPopulation=50000&namePrefix=${inputValue}`, geodbApiOptions)
-            .then((response) => response.json())
-            .then((response) => {
-                return {
-                    options: response.data.map((city) => {
-                        return {
-                            label: `${city.name}, ${city.region}, ${city.country}, ${city.countryCode}`,
-                            value: `${city.latitude} ${city.longitude}`,
-                        };
-                    }),
-                };
-            })
-            .catch((err) => console.error(err));
-    };
-
-    /* retrieve data from asyncPaginate componen, searchCityData */
-    const handleOnChange = (searchCityData) => {
-        setCitySearch(searchCityData); /* update our search with new value*/
-        onSearchCityChange(searchCityData); /* pass the city search data received from input */
-    };
-
-    // what the user sees
-    return (
-        <AsyncPaginate
-            placeholder="Begin typing a city's name to autocomplete. Overwrite a city to continue searching :"
-            debounceTimeout={600}/* slows API requests in ms */
-            value={citysearch} /* user input value of city name */
-            onChange={handleOnChange} /* update value from App.js to pass info to other 2 widgets*/
-            loadOptions={loadCityOptions} /* load city long+lat from API */
-        />
+  // using fetch method to url to retrieve data from GeoDB API; adding constraint to show cities with population over value listed and showing results of inputValue
+  const loadCityOptions = (inputValue) => {
+    // promise chain instead of async/await
+    // inner return: return array of objects with long & lat as OpenWeatherMap API requires it to retrieve weather data
+    return fetch(
+      `${geodb_api_url}/cities?sort=-US&minPopulation=100000&namePrefix=${inputValue}`,
+      geodbApiOptions
     )
-}
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              label: `${city.name}, ${city.region}, ${city.country}, ${city.countryCode}`,
+              value: `${city.latitude} ${city.longitude}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
+
+  /* retrieve data from asyncPaginate component, searchCityData */
+  const handleOnChange = (searchCityData) => {
+    setCitySearch(searchCityData); /* update search with new value*/
+    onSearchCityChange(
+      searchCityData
+    ); /* pass the city search data received from input */
+  };
+
+  // what the user sees
+  return (
+    <>
+      <br></br>
+      <br></br>
+      <h2>Search Weather by City Name</h2>
+      <label>
+        NOTE: Typing out more letters can give you better results. Autocomplete
+        is limited to city population of 100,000 inhabitants. Duplicates may
+        occur.
+        <br></br>
+        <br></br>
+        TO USE: Backspace to edit. To begin a new search, click on the input
+        field and overwrite the prompt with a new city name.
+      </label>
+
+      <div></div>
+      <div></div>
+
+      <AsyncPaginate
+        placeholder="Click here to begin typing a city's name which features autocomplete... Backspace to edit city..."
+        debounceTimeout={600} /* slows API requests in ms */
+        value={citysearch} /* user input value of city name */
+        onChange={
+          handleOnChange
+        } /* update value from App.js to pass info to other 2 widgets*/
+        loadOptions={loadCityOptions} /* load city long+lat from API */
+      />
+
+      <button className="cityClear" onClick={() => setCitySearch("")}>
+        Clear Entry
+      </button>
+    </>
+  );
+};
 
 export default CitySearch;
 
---
+++ src/components/zipsearch/zipsearch.js
@@ -0,0 +1,134 @@
+// Search component for zipcode search on top of the screen using the async paginate package
+
+import { useState } from "react";
+// import { AsyncPaginate } from "react-select-async-paginate";
+import {
+  OPENWEATHER_API_KEY,
+  OPENWEATHER_API_URL,
+  OPENWEATHER_API_URL_ZIPCODE_CONVERSION,
+} from "../../api";
+// driven by OpenWeather Geo API for zipcode search to obtain lat/long data
+
+// action which drives the zipcode search in passing data to other widgets
+const ZipcodeSearch = ({ setCurrentWeather, setForecastWeather }) => {
+  // use state hooks variables with set for updating variable
+  const [zipcodesearch, setZipcodeSearch] = useState(null); // initial state is null
+
+  // using fetch method to url to retrieve data from OPENWEATHER GEO API for zipcode search-showing results of zipcodeValue
+  const fetchWeatherByZipcode = async () => {
+    // promise chain instead of async/await
+    // inner return: return array of objects with long & lat as OpenWeatherMap API requires it to retrieve weather data
+    // fetch(url, options) where options is {method: "GET"} as default so fetch(url) ok.
+    // // fetch(
+    //     `${OPENWEATHER_API_URL}/zip?zip=${zipcodeValue}&appid=${OPENWEATHER_API_KEY}`)
+
+    // label: `${city.zip}, ${city.name}, ${city.country}`,
+    //         value: `${city.lat} ${city.lon}`,
+
+    const response = await fetch(
+      `${OPENWEATHER_API_URL_ZIPCODE_CONVERSION}/zip?zip=${zipcodesearch},US&appid=${OPENWEATHER_API_KEY}`
+    );
+
+    const geodata = await response.json();
+
+    const weatherResponse = await fetch(
+      `${OPENWEATHER_API_URL}/weather?lat=${geodata.lat}&lon=${geodata.lon}&appid=${OPENWEATHER_API_KEY}&units=imperial`
+    );
+    const weatherData = await weatherResponse.json();
+
+    //  how to Format the weatherData  and label value
+    // label: `${weatherData.zip}, ${weatherData.name}, ${weatherData.country}`,
+    // value: `${weatherData.coord.lat} ${weatherData.coord.lon}`,
+
+    // console.log(geodata);
+    // console.log(weatherData);
+
+    // update the responses to the state via setVar so data can be passed to the components & using spread operator to add data from citysearch.js
+    setCurrentWeather({
+      city: weatherData.name,
+      ...weatherData,
+    }); // pulling from vars above & adding data from citysearch.js so labels appear
+    setForecastWeather({
+      city: weatherData.name,
+      ...weatherData,
+    });
+    //   return fetch(${OPENWEATHER_API_URL})
+  };
+
+  /* retrieve data from asyncPaginate component, searchCityData */
+  const handleOnChange = (e) => {
+    setZipcodeSearch(e.target.value); /* update our search with new value*/
+
+    //testing
+    // console.log(searchZipcodeData);
+  };
+
+  // what the user sees
+  // changed type=number to type=text
+  return (
+    <>
+      <br></br>
+      <br></br>
+      <div></div>
+      <div></div>
+      <br></br>
+
+      <h2>Search Weather by Zipcode</h2>
+      <div></div>
+      <div>
+        <label>Enter a U.S. zipcode:</label>
+        <input
+          name="zipcode"
+          type="text"
+          onChange={handleOnChange}
+          value={zipcodesearch}
+        />
+        <button type="submit" onClick={fetchWeatherByZipcode}>
+          Submit
+        </button>
+
+        <button className="zipClear" onClick={() => setZipcodeSearch("")}>
+          Clear Entry
+        </button>
+      </div>
+      <br></br>
+    </>
+  );
+};
+
+export default ZipcodeSearch;
+
+// initially tested with dummy component: return('Hello World!')
+// replaced with asyncPaginate placeholder to search for cities.
+// returns/get long and lat data from OpenWeather Geo API after a user enters in a zipcode
+
+//  const loadZipcodeOptions = (zipcodeValue) => {
+
+//         // promise chain instead of async/await
+//         // inner return: return array of objects with long & lat as OpenWeatherMap API requires it to retrieve weather data
+//         // fetch(url, options) where options is {method: "GET"} as default so fetch(url) ok.
+//         // // fetch(
+//         //     `${OPENWEATHER_API_URL}/zip?zip=${zipcodeValue}&appid=${OPENWEATHER_API_KEY}`)
+//         return fetch(
+//             `${OPENWEATHER_API_URL_ZIPCODE_CONVERSION}/weather?zip=${zipcodeValue}&appid=${OPENWEATHER_API_KEY}`)
+//             .then((response) => response.json())
+//             .then((response) => {
+//                 return {
+//                     options: response.data.map((city) => {
+//                         return {
+//                             label: `${city.zip}, ${city.name}, ${city.country}`,
+//                             value: `${city.lat} ${city.lon}`,
+//                         };
+//                     }),
+//                 };
+//             })
+//             .catch((err) => console.error(err));
+//     };
+
+// {/* <AsyncPaginate
+// placeholder="Begin typing a city's zipcode :"
+// debounceTimeout={600}/* slows API requests in ms */
+// value={zipcodesearch} /* user input value of city zipcode */
+// onChange={handleOnChange} /* update value from App.js to pass info to other 2 widgets*/
+// loadOptions={loadZipcodeOptions} /* load city long+lat from API */
+// /> */}
