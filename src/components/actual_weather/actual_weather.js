fcc9b86c9ebd8c247798908d9340395f1d5b44bd
sajidahw <sajidahwahdy@Sajidahs-MacBook-Air.local>
2024-02-10 19:02:22 -0800

    updated weather data card to display for both citynames and zipcodes

--- src/components/actual_weather/actual_weather.js
+++ src/components/actual_weather/actual_weather.js
@@ -1,6 +1,4 @@
-
-
-import "./actual_weather.css"
+import "./actual_weather.css";
 
 // dummy component to integrate inside of app.js file to start building
 // card display of current weather data
@@ -8,53 +6,63 @@ import "./actual_weather.css"
 // downloaded icons for static testing: src="../icons/50d.png"
 // downloaded icons from folder: src={`icons/${data.weather[0].icon}.png`}
 // icons from API: src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`}
-const ActualWeather = ({data}) => {
-    return (
-        <div className="actualweather_card">
-            <div className="actualweather_contents">
-                <div>
-                    <p className="city-name">{data.city}</p>
-                    <p className="weather-description">{data.weather[0].description}</p>
-                </div>
-                
-                <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`}/>
-                
-            </div>
-            
-            <div className="bottom_specifics">
-                <p className="temperature">{Math.round(data.main.temp)}°F</p>
+const ActualWeather = ({ data }) => {
+  return (
+    <>
+      <br></br>
 
-                <div className="details">
-                    <div className="parameter-row">
-                        <span className="parameter-label top">Details: </span>
-                    </div>
+      <h2 className="currentweatherH2">Current Weather</h2>
+      <br></br>
 
-                    <div className="parameter-row">
-                        <span className="parameter-label">Feels like: </span>
-                        <span className="parameter-value">{Math.round(data.main.feels_like)}°F</span>
-                    </div>
+      <div className="actualweather_card">
+        <div className="actualweather_contents">
+          <div>
+            <p className="city-name">{data.city}</p>
+            <p className="weather-description">{data.weather[0].description}</p>
+          </div>
 
-                    <div className="parameter-row">
-                        <span className="parameter-label">Wind:  </span>
-                        <span className="parameter-value">{data.wind.speed} mph</span>
-                    </div>
-
-                    <div className="parameter-row">
-                        <span className="parameter-label">Humidity:  </span>
-                        <span className="parameter-value">{data.main.humidity}%</span>
-                    </div>
-
-                    <div className="parameter-row">
-                        <span className="parameter-label">Pressure:  </span>
-                        <span className="parameter-value">{data.main.pressure} inHg</span>
-                    </div>
-
-                </div>
-
-            </div>
+          <img
+            alt="weather"
+            className="weather-icon"
+            src={`icons/${data.weather[0].icon}.png`}
+          />
         </div>
-    );
-}
+
+        <div className="bottom_specifics">
+          <p className="temperature">{Math.round(data.main.temp)}°F</p>
+
+          <div className="details">
+            <div className="parameter-row">
+              <span className="parameter-label top">Details: </span>
+            </div>
+
+            <div className="parameter-row">
+              <span className="parameter-label">Feels like: </span>
+              <span className="parameter-value">
+                {Math.round(data.main.feels_like)}°F
+              </span>
+            </div>
+
+            <div className="parameter-row">
+              <span className="parameter-label">Wind: </span>
+              <span className="parameter-value">{data.wind.speed} mph</span>
+            </div>
+
+            <div className="parameter-row">
+              <span className="parameter-label">Humidity: </span>
+              <span className="parameter-value">{data.main.humidity}%</span>
+            </div>
+
+            <div className="parameter-row">
+              <span className="parameter-label">Pressure: </span>
+              <span className="parameter-value">{data.main.pressure} inHg</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default ActualWeather;
 
